662002ba96d68226a92159089c08f516e64a17f6
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-17 19:27:51 -0500

    setting users up

---
+++ NESVault_be/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NESVault_be.Controllers
+{
+    public class UsersController
+    {
+    }
+}
