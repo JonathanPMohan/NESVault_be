f12d6f6e82dab0ccd59bcf992af8dcda05a95f6b
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-23 00:49:42 -0500

    built cart models

---
+++ NESVault_be/Models/UpdateCartRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NESVault_be.Models
+{
+    public class UpdateCartRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Loose { get; set; }
+        public decimal Cib { get; set; }
+        public decimal New { get; set; }
+    }
+}
