8659356bfb40216057a7ae39fbab2fe4c2a2dd64
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-26 02:30:39 -0500

    built wish list models

---
+++ NESVault_be/Models/UpdateWishListRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NESVault_be.Models
+{
+    public class UpdateWishListRequest
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CartId { get; set; }
+    }
+}
